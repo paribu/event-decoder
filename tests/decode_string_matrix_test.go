1e41d10e3499b47cf1382b026b96ca714043dddd
hyenacoder <hyenacoder@mail.com.tr>
2024-01-30 17:10:57 +0300

    test: added unit test case for decoding string matrix process

---
+++ tests/decode_string_matrix_test.go
@@ -0,0 +1,67 @@
+package tests
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/paribu/event-decoder/event"
+)
+
+type Matrix struct {
+	Values [][]string `json:"values"`
+}
+
+type MatrixParameter struct {
+	Type  string     `json:"type"`
+	Name  string     `json:"name"`
+	Value [][]string `json:"value"`
+}
+
+func TestDecodeStringMatrix(t *testing.T) {
+	abiFile := "decode_string_matrix_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0x0be2a4eb55a582e5b98fb794c856287787d84edda88b1dc4876dc07eb7e981c2"),
+		},
+		Data: "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000120000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000568656c6c6f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005776f726c640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000004676f6f640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000046c75636b00000000000000000000000000000000000000000000000000000000",
+	}
+
+	expectedParameters := []MatrixParameter{
+		{
+			Type: "string[][]",
+			Name: "newValue",
+			Value: [][]string{
+				{"hello", "world"},
+				{"good", "luck"},
+			},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedMatrix Matrix
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedMatrix); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedMatrix.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedMatrix.Values)
+		}
+	}
+}
