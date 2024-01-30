045e63cabeb4341af962bd4d1b43d509083d961e
hyenacoder <hyenacoder@mail.com.tr>
2024-01-30 12:49:15 +0300

    test: added unit test case for decoding int matrix process

---
+++ tests/decode_int_matrix_test.go
@@ -0,0 +1,67 @@
+package tests
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/paribu/event-decoder/event"
+)
+
+type Matrix struct {
+	Values [][]string `json:"values"`
+}
+
+type MatrixParameter struct {
+	Type  string     `json:"type"`
+	Name  string     `json:"name"`
+	Value [][]string `json:"value"`
+}
+
+func TestDecodeIntMatrix(t *testing.T) {
+	abiFile := "decode_int_matrix_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0x5c1bacf54dafce3497c26f76c8391ef2971c09102c841e47d96848150be4fdd8"),
+		},
+		Data: "00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000010fa4a62c4dffff90000000000000000000000000000000000000000000000001edb01166c43fffa00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000002cbbb7ca13a7fffb0000000000000000000000000000000000000000000000003a9c6e7dbb0bfffc",
+	}
+
+	expectedParameters := []MatrixParameter{
+		{
+			Type: "int256[][]",
+			Name: "newValue",
+			Value: [][]string{
+				{"1223372036854775801", "2223372036854775802"},
+				{"3223372036854775803", "4223372036854775804"},
+			},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedMatrix Matrix
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedMatrix); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedMatrix.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedMatrix.Values)
+		}
+	}
+}
