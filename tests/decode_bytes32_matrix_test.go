c962dcd791bb2bb5e5b3ad86e2e663189ebf4d41
hyenacoder <hyenacoder@mail.com.tr>
2024-01-30 14:03:38 +0300

    test: added unit test case for decoding bytes32 matrix process

---
+++ tests/decode_bytes32_matrix_test.go
@@ -0,0 +1,95 @@
+package tests
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/paribu/event-decoder/event"
+)
+
+type Matrix struct {
+	Values [][]string `json:"values"`
+}
+
+type MatrixParameter struct {
+	Type  string     `json:"type"`
+	Name  string     `json:"name"`
+	Value [][]string `json:"value"`
+}
+
+func TestDecodeBytes32Matrix(t *testing.T) {
+	abiFile := "decode_bytes32_matrix_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0xd0675676ee0952ac2c3f0c55d267ec392734bb465a5273d849f4c186c88d576e"),
+		},
+		Data: "00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000230783163366637323635366432303639373037333735366432303636363631313078326336663732363536643230363937303733373536643230363636363232000000000000000000000000000000000000000000000000000000000000000230783363366637323635366432303639373037333735366432303636363633333078346336663732363536643230363937303733373536643230363636363434",
+	}
+
+	expectedParameters := []MatrixParameter{
+		{
+			Type: "bytes32[][]",
+			Name: "newValue",
+			Value: [][]string{
+				{"0x1c6f72656d20697073756d20666611", "0x2c6f72656d20697073756d20666622"},
+				{"0x3c6f72656d20697073756d20666633", "0x4c6f72656d20697073756d20666644"},
+			},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedMatrix Matrix
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedMatrix); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		decodedMatrixAscii := convertHexToAscii(t, decodedMatrix)
+		if !reflect.DeepEqual(expected.Value, decodedMatrixAscii) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedMatrixAscii)
+		}
+	}
+}
+
+func convertHexToAscii(t *testing.T, decodedMatrix Matrix) [][]string {
+	rows := len(decodedMatrix.Values)
+	cols := len(decodedMatrix.Values[0])
+
+	result := make([][]string, rows)
+
+	for i := 0; i < rows; i++ {
+		result[i] = make([]string, cols)
+
+		for j := 0; j < cols; j++ {
+			hexString := decodedMatrix.Values[i][j]
+			hexString = hexString[2:]
+
+			hexBytes, err := hex.DecodeString(hexString)
+			if err != nil {
+				t.Error(err)
+				return nil
+			}
+
+			result[i][j] = string(hexBytes)
+		}
+	}
+
+	return result
+}
