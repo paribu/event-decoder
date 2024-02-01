e9da679b698b1cfda6cc1d214b68871a72dcb042
Mehmet Dağlı <mehmet.dagli@xprclub.com>
2024-02-01 15:49:26 +0300

    chore: added unit test case for decoding uint array types

--- tests/decode_uint_array_test.go
+++ tests/decode_uint_array_test.go
@@ -9,8 +9,223 @@ import (
 	"github.com/paribu/event-decoder/event"
 )
 
-func TestDecodeUintArray(t *testing.T) {
-	abiFile := "decode_uint_array_abi.json"
+func TestDecodeUint8Array(t *testing.T) {
+	abiFile := "./decode-uint-abi/decode_uint8_array_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0x76640e0454e37adebc7db07bebbf0a58e839afcd9d16633bde2e97ef58abb28a"),
+		},
+		Data: "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000ff00000000000000000000000000000000000000000000000000000000000000fe",
+	}
+
+	expectedParameters := []ArrayParameter{
+		{
+			Type:  "uint8[]",
+			Name:  "newValue",
+			Value: []string{"255", "254"},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedArray Array
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedArray); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedArray.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedArray.Values)
+		}
+	}
+}
+
+func TestDecodeUint24Array(t *testing.T) {
+	abiFile := "./decode-uint-abi/decode_uint24_array_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0x61d6f1ccffbf1f5cf9e52c7f0f401939894f101e19a5bf4bf87eb4d6b774c736"),
+		},
+		Data: "000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000ffffff0000000000000000000000000000000000000000000000000000000000fffffe",
+	}
+
+	expectedParameters := []ArrayParameter{
+		{
+			Type:  "uint24[]",
+			Name:  "newValue",
+			Value: []string{"16777215", "16777214"},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedArray Array
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedArray); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedArray.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedArray.Values)
+		}
+	}
+}
+
+func TestDecodeUint32Array(t *testing.T) {
+	abiFile := "./decode-uint-abi/decode_uint32_array_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0xf9593a566df02e076cadd0dc9fdc7a2f68124d2f1f3cb53d2e4fcc2f70abfe1e"),
+		},
+		Data: "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000ffffffff00000000000000000000000000000000000000000000000000000000fffffffe",
+	}
+
+	expectedParameters := []ArrayParameter{
+		{
+			Type:  "uint32[]",
+			Name:  "newValue",
+			Value: []string{"4294967295", "4294967294"},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedArray Array
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedArray); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedArray.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedArray.Values)
+		}
+	}
+}
+
+func TestDecodeUint40Array(t *testing.T) {
+	abiFile := "./decode-uint-abi/decode_uint40_array_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0x6f6fb5d65cb0be2fe170574db1b48aca3e49faafb44bc5e6558f3a9361ab66c3"),
+		},
+		Data: "00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000ffffffffff000000000000000000000000000000000000000000000000000000fffffffffe",
+	}
+
+	expectedParameters := []ArrayParameter{
+		{
+			Type:  "uint40[]",
+			Name:  "newValue",
+			Value: []string{"1099511627775", "1099511627774"},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedArray Array
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedArray); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedArray.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedArray.Values)
+		}
+	}
+}
+
+func TestDecodeUint64Array(t *testing.T) {
+	abiFile := "./decode-uint-abi/decode_uint64_array_abi.json"
+
+	encodedEvent := &event.Event{
+		Topics: []common.Hash{
+			common.HexToHash("0xd77aedeb4984236c8ec1d875c89b8e3cf7e01203f0ef7ea5f7ee35c279d8c92e"),
+		},
+		Data: "00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000ffffffffffffffff000000000000000000000000000000000000000000000000fffffffffffffffe",
+	}
+
+	expectedParameters := []ArrayParameter{
+		{
+			Type:  "uint64[]",
+			Name:  "newValue",
+			Value: []string{"18446744073709551615", "18446744073709551614"},
+		},
+	}
+
+	parameters, err := decodeEventWithABI(abiFile, encodedEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, decoded := range parameters {
+		expected := expectedParameters[i]
+		if expected.Name != decoded.Name {
+			t.Errorf("expected name %s, got %s", expected.Name, decoded.Name)
+		}
+		if expected.Type != decoded.Type {
+			t.Errorf("expected type %s, got %s", expected.Type, decoded.Type)
+		}
+
+		var decodedArray Array
+		if err := json.Unmarshal([]byte(decoded.Value), &decodedArray); err != nil {
+			t.Errorf("error decoding JSON: %v", err)
+		}
+
+		if !reflect.DeepEqual(expected.Value, decodedArray.Values) {
+			t.Errorf("expected value %v, got %v", expected.Value, decodedArray.Values)
+		}
+	}
+}
+
+func TestDecodeUint256Array(t *testing.T) {
+	abiFile := "./decode-uint-abi/decode_uint256_array_abi.json"
 
 	encodedEvent := &event.Event{
 		Topics: []common.Hash{
